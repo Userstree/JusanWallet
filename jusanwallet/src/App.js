915950146d09b9e12b17adb070d8b0f460568436
lazzetnur <lchakyyeva@gmail.com>
2022-07-08 15:42:04 +0600

    left menu

--- jusanwallet/src/App.js
+++ jusanwallet/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Menu from './components/Menu'
 
 function App() {
   return (
     <div className='App'>
+      <Menu />
       <header className='App-header'>
-        <h1>Jusan Wallet</h1>
+      
       </header>
     </div>
   );
---
+++ jusanwallet/src/components/Menu/index.jsx
@@ -0,0 +1,25 @@
+import './menu.css';
+import { AiFillHome, AiOutlineHistory, AiFillContacts, AiFillSetting } from 'react-icons/ai';
+import { SiContactlesspayment } from 'react-icons/si';
+import { IoIosNotifications } from 'react-icons/io';
+import { CgProfile } from 'react-icons/cg';
+
+
+function Menu() {
+  return (
+    <div className='Menu'>
+      <a class='active' href='#home'>
+        <AiFillHome/>
+      </a>
+      <a href='#history'><AiOutlineHistory /></a>
+      <a href='#auto'><SiContactlesspayment /></a>
+      <a href='#contact'><AiFillContacts /></a>
+      <a href='#contact'><IoIosNotifications /></a>
+      <a href='#contact'><CgProfile /></a>
+      <a href='#contact'><AiFillSetting /></a>
+
+    </div>
+  );
+}
+
+export default Menu;
